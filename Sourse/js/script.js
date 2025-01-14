b6d6642bb442c91b96c7231ef83b8f268a365ee2
a-zub <slanecru@outlook.com>
2025-01-14 19:26:06 +0300

    First commit

---
+++ Sourse/js/script.js
@@ -0,0 +1,7 @@
+//@prepros-append jquery-3.7.1.js
+//@prepros-append ibg.js
+//@prepros-append main.js
+
+
+
+
---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+
+function ibg() {
+
+    let ibg = document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+        if (ibg[i].querySelector('img')) {
+            ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+        }
+    }
+}
+
+ibg();
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const   menu = document.querySelector('.menu'),
+			menuItem = document.querySelectorAll('.menu__item'),
+			hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger__active');
+        menu.classList.toggle('menu__active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger__active');
+            menu.classList.toggle('menu__active');
+        })
+    })
+})
+
+
+
+
+
+
+
